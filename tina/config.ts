bd3be3c5153e190a5216aef53725128072e4202f
Samuel <samuel.drilleau@tutanota.com>
2025-05-15 19:24:25 +0200

    Add tina

---
+++ tina/config.ts
@@ -0,0 +1,52 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch =
+  process.env.GITHUB_BRANCH ||
+  process.env.VERCEL_GIT_COMMIT_REF ||
+  process.env.HEAD ||
+  "main";
+
+export default defineConfig({
+  branch,
+
+  // Get this from tina.io
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+  // Get this from tina.io
+  token: process.env.TINA_TOKEN,
+
+  client: { skip: true },
+  build: {
+    outputFolder: "admin",
+    publicFolder: "assets",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "assets",
+    },
+  },
+  // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
+  schema: {
+    collections: [
+      {
+        format: "md",
+        label: "Données",
+        name: "donn_es",
+        path: "_data",
+        match: {
+          include: "**/*",
+        },
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
