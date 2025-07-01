cb7c1ecba3633fe52fdcc966baf3d1dfe808abd5
Samuel <samuel.drilleau@tutanota.com>
2025-07-01 23:20:06 +0200

    fix: tina admin

--- tina/config.ts
+++ tina/config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   client: { skip: true },
   build: {
     outputFolder: "admin",
-    publicFolder: "public",
+    publicFolder: "/",
     basePath: "Site"
   },
   media: {
